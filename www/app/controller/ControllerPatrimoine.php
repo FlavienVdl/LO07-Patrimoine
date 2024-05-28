151bf02965e6f3d04daf5dfb7bec4e22dd5fd9a2
Flavien <flavien.vidal@etu.univ-orleans.fr>
2024-05-28 19:43:14 +0200

    [A] Ajout interfaces patrimoine

---
+++ www/app/controller/ControllerPatrimoine.php
@@ -0,0 +1,18 @@
+
+<!-- ----- debut ControllerPatrimoine -->
+<?php
+
+class ControllerPatrimoine {
+    // --- page d'accueil
+ public static function patrimoineAccueil() {
+    include 'config.php';
+    $vue = $root . '/app/view/viewPatrimoineAccueil.php';
+    if (DEBUG)
+     echo ("ControllerPatrimoine : patrimoineAccueil : vue = $vue");
+    require ($vue);
+   }
+}
+?>
+<!-- ----- fin ControllerPatrimoine -->
+
+
